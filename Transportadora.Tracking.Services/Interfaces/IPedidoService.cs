6847c9e74d8ae565db793c160ee0569bc063417d
Gustavo da Silva <gustavaquinooficial@gmail.com>
2021-11-24 19:02:59 -0300

    Refatorando o código da funcionalidade de obter pedidos.

--- Transportadora.Tracking.Services/Interfaces/IPedidoService.cs
+++ Transportadora.Tracking.Services/Interfaces/IPedidoService.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Transportadora.Tracking.Entities.Models.InputModel;
 using Transportadora.Tracking.Entities.Models.ViewModel;
+using Transportadora.Tracking.Entities.Table;
 
 namespace Transportadora.Tracking.Services.Interfaces
 {
@@ -8,10 +10,20 @@ namespace Transportadora.Tracking.Services.Interfaces
     {
         Task Inserir(PedidoInputModel pedido);
 
+        Task<PedidoViewModel> Obter(string codigoPedido);
+
         Task<bool> PedidoExiste(string codigoPedido);
 
         Task<int> RetornaPedidoId(string codigoPedido);
 
-        Task<PedidoViewModel> Obter(string codigoPedido);
+        Task<RemetenteTable> ObterRemetente(string codigoPedido);
+
+        Task<DestinatarioTable> ObterDestinatario(string codigoPedido);
+
+        Task<IEnumerable<ItemsTable>> ObterItems(string codigoPedido);
+
+        Task<PedidoTable> ObterPedido(string codigoPedido);
+
+        Task<EnderecoTable> ObterEndereco(int enderecoId, string codigoPedido);
     }
 }
--- Transportadora.Tracking.Services/PedidoService.cs
+++ Transportadora.Tracking.Services/PedidoService.cs
@@ -8,6 +8,7 @@ using Transportadora.Tracking.Services.Interfaces;
 using Transportadora.Tracking.Services.CustomExceptions;
 using Transportadora.Tracking.Entities.Models.ViewModel;
 using System.Linq;
+using Transportadora.Tracking.Entities.Table;
 
 namespace Transportadora.Tracking.Services
 {
@@ -29,8 +30,6 @@ namespace Transportadora.Tracking.Services
             try
             {
                 int remetenteId, destinatarioId;
-
-                #region InstanciandoObjetos
 
                 var items = new List<Item>();
 
@@ -54,7 +53,7 @@ namespace Transportadora.Tracking.Services
                         Empresa = pedido.Destintario.Empresa,
                         Cnpj = (pedido.Destintario.Empresa) ? pedido.Destintario.Cnpj : string.Empty,
                         Cpf = (pedido.Destintario.Empresa) ? string.Empty : pedido.Destintario.Cpf,
-                        Endereco  = new Endereco
+                        Endereco = new Endereco
                         {
                             Cep = pedido.Destintario.Endereco.Cep,
                             Logradouro = pedido.Destintario.Endereco.Logradouro,
@@ -85,10 +84,8 @@ namespace Transportadora.Tracking.Services
                     Items = items
                 };
 
-                #endregion InstanciandoObjetos
-
                 remetenteId = await _pedidoRepository.RemetenteCadastrado(pedidoInput.Remetente);
-                
+
                 if (remetenteId == 0)
                 {
                     var remetenteEnderecoId = await _pedidoRepository.CadastrarEndereco(pedidoInput.Remetente.Endereco);
@@ -150,7 +147,6 @@ namespace Transportadora.Tracking.Services
 
         public async Task<PedidoViewModel> Obter(string codigoPedido)
         {
-            var pedidoRetorno = new PedidoViewModel();
             var itemsRetorno = new List<ItemViewModel>();
 
             try
@@ -158,124 +154,70 @@ namespace Transportadora.Tracking.Services
                 if (!await _pedidoRepository.PedidoExiste(codigoPedido))
                     throw new NotFoundException($"Pedido {codigoPedido} não encontrado.");
 
-                var rementente = await _pedidoRepository.ObterRemetente(codigoPedido);
+                var rementente = await ObterRemetente(codigoPedido);
 
-                if (rementente != null)
+                var destinatario = await ObterDestinatario(codigoPedido);
+
+                var items = await ObterItems(codigoPedido);
+
+                foreach (var item in items)
                 {
-                    var destinatario = await _pedidoRepository.ObterDestinatario(codigoPedido);
-
-                    if (destinatario != null)
+                    itemsRetorno.Add(new ItemViewModel
                     {
-                        var items = await _pedidoRepository.ObterItems(codigoPedido);
-
-                        if (items.Any())
-                        {
-                            var remetenteRetorno = new RemetenteViewModel
-                            {
-                                RazaoSocial = rementente.RazaoSocial,
-                                Empresa = rementente.Empresa == 1 ? true : false,
-                                Cnpj = rementente.Cnpj,
-                                Cpf = rementente.Cpf
-                            };
-
-                            pedidoRetorno.Remetente = remetenteRetorno;
-
-                            var destinatarioRetorno = new DestinatarioViewModel
-                            {
-                                RazaoSocial = destinatario.RazaoSocial,
-                                Empresa = destinatario.Empresa == 1 ? true : false,
-                                Cnpj = destinatario.Cnpj,
-                                Cpf = destinatario.Cpf
-                            };
-
-                            pedidoRetorno.Destintario = destinatarioRetorno;
-
-                            foreach (var item in items)
-                            {
-                                itemsRetorno.Add(new ItemViewModel
-                                    {
-                                        Codigo = item.Codigo,
-                                        Descricao = item.Descricao,
-                                        Quantidade = item.Quantidade,
-                                        Preco = Convert.ToString(item.Preco)
-                                    }
-                                );
-                            }
-
-                            pedidoRetorno.Items = itemsRetorno;
-
-                            var pedido = await _pedidoRepository.ObterPedido(codigoPedido);
-
-                            if (pedido != null)
-                            {
-                                pedidoRetorno.PedidoID = pedido.PedidoID;
-                                pedidoRetorno.CodigoPedido = pedido.CodigoPedido;
-                                pedidoRetorno.DataIntegracao = pedido.DataCadastro.ToString("yyyy-MM-dd HH-mm-ss");
-
-                                var enderecoRemetente = await _pedidoRepository.ObterEndereco(rementente.EnderecoID);
-
-                                if (enderecoRemetente != null)
-                                {
-                                    var enderecoDestinatario = await _pedidoRepository.ObterEndereco(destinatario.EnderecoID);
-
-                                    if (enderecoDestinatario != null)
-                                    {
-                                        var enderecoDestinatarioRetorno = new EnderecoViewModel
-                                        {
-                                            Cep = enderecoDestinatario.Cep,
-                                            Logradouro = enderecoDestinatario.Logradouro,
-                                            Numero = enderecoDestinatario.Numero,
-                                            Bairro = enderecoDestinatario.Bairro,
-                                            Cidade = enderecoDestinatario.Cidade,
-                                            Estado = enderecoDestinatario.Estado,
-                                            Pais = enderecoDestinatario.Pais
-                                        };
-
-                                        var enderecoRemetenteRetorno = new EnderecoViewModel
-                                        {
-                                            Cep = enderecoRemetente.Cep,
-                                            Logradouro = enderecoRemetente.Logradouro,
-                                            Numero = enderecoRemetente.Numero,
-                                            Bairro = enderecoRemetente.Bairro,
-                                            Cidade = enderecoRemetente.Cidade,
-                                            Estado = enderecoRemetente.Estado,
-                                            Pais = enderecoRemetente.Pais
-                                        };
-
-                                        pedidoRetorno.Destintario.Endereco = enderecoDestinatarioRetorno;
-                                        pedidoRetorno.Remetente.Endereco = enderecoRemetenteRetorno;
-                                    }
-                                    else
-                                    {
-                                        throw new NotFoundException($"Não foi encontrado endereco de destinatário para o pedido {codigoPedido}.");
-                                    }
-                                }
-                                else
-                                {
-                                    throw new NotFoundException($"Não foi encontrado endereco de remetente para o pedido {codigoPedido}.");
-                                }
-                            }
-                            else
-                            {
-                                throw new NotFoundException($"Cabeçalho do pedido {codigoPedido} inexistente.");
-                            }
-                        }
-                        else
-                        {
-                            throw new NotFoundException($"Pedido {codigoPedido} não possui items.");
-                        }
-                    }
-                    else
-                    {
-                        throw new NotFoundException($"Não foi encontrado destinatário cadastrado para o pedido {codigoPedido}.");
-                    }
-                }
-                else
-                {
-                    throw new NotFoundException($"Não foi encontrado remetente cadastrado para o pedido {codigoPedido}.");
+                        Codigo = item.Codigo,
+                        Descricao = item.Descricao,
+                        Quantidade = item.Quantidade,
+                        Preco = Convert.ToString(item.Preco)
+                    });
                 }
 
-                return pedidoRetorno;
+                var pedido = await ObterPedido(codigoPedido);
+
+                var enderecoRemetente = await ObterEndereco(rementente.EnderecoID, codigoPedido);
+
+                var enderecoDestinatario = await ObterEndereco(destinatario.EnderecoID, codigoPedido);
+
+                return new PedidoViewModel
+                {
+                    PedidoID = pedido.PedidoID,
+                    CodigoPedido = pedido.CodigoPedido,
+                    DataIntegracao = pedido.DataCadastro.ToString("yyyy-MM-dd HH-mm-ss"),
+                    Remetente = new RemetenteViewModel
+                    {
+                        RazaoSocial = rementente.RazaoSocial,
+                        Empresa = rementente.Empresa == 1 ? true : false,
+                        Cnpj = rementente.Cnpj,
+                        Cpf = rementente.Cpf,
+                        Endereco = new EnderecoViewModel
+                        {
+                            Cep = enderecoRemetente.Cep,
+                            Logradouro = enderecoRemetente.Logradouro,
+                            Numero = enderecoRemetente.Numero,
+                            Bairro = enderecoRemetente.Bairro,
+                            Cidade = enderecoRemetente.Cidade,
+                            Estado = enderecoRemetente.Estado,
+                            Pais = enderecoRemetente.Pais
+                        }
+                    },
+                    Destintario = new DestinatarioViewModel
+                    {
+                        RazaoSocial = destinatario.RazaoSocial,
+                        Empresa = destinatario.Empresa == 1 ? true : false,
+                        Cnpj = destinatario.Cnpj,
+                        Cpf = destinatario.Cpf,
+                        Endereco = new EnderecoViewModel
+                        {
+                            Cep = enderecoDestinatario.Cep,
+                            Logradouro = enderecoDestinatario.Logradouro,
+                            Numero = enderecoDestinatario.Numero,
+                            Bairro = enderecoDestinatario.Bairro,
+                            Cidade = enderecoDestinatario.Cidade,
+                            Estado = enderecoDestinatario.Estado,
+                            Pais = enderecoDestinatario.Pais
+                        }
+                    },
+                    Items = itemsRetorno
+                };
             }
             catch (Exception)
             {
@@ -300,6 +242,91 @@ namespace Transportadora.Tracking.Services
             try
             {
                 return await _pedidoRepository.RetornaPedidoId(codigoPedido);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<RemetenteTable> ObterRemetente(string codigoPedido)
+        {
+            try
+            {
+                var rementente = await _pedidoRepository.ObterRemetente(codigoPedido);
+
+                if (rementente == null)
+                    throw new NotFoundException($"Não foi encontrado remetente cadastrado para o pedido {codigoPedido}.");
+
+                return rementente;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<DestinatarioTable> ObterDestinatario(string codigoPedido)
+        {
+            try
+            {
+                var destinatario = await _pedidoRepository.ObterDestinatario(codigoPedido);
+
+                if (destinatario == null)
+                    throw new NotFoundException($"Não foi encontrado destinatário cadastrado para o pedido {codigoPedido}.");
+
+                return destinatario;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<ItemsTable>> ObterItems(string codigoPedido)
+        {
+            try
+            {
+                var items = await _pedidoRepository.ObterItems(codigoPedido);
+
+                if (!items.Any())
+                    throw new NotFoundException($"Pedido {codigoPedido} não possui items.");
+
+                return items;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<PedidoTable> ObterPedido(string codigoPedido)
+        {
+            try
+            {
+                var pedido = await _pedidoRepository.ObterPedido(codigoPedido);
+
+                if (pedido == null)
+                    throw new NotFoundException($"Cabeçalho do pedido {codigoPedido} inexistente.");
+
+                return pedido;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<EnderecoTable> ObterEndereco(int enderecoId, string codigoPedido)
+        {
+            try
+            {
+                var enderecoRemetente = await _pedidoRepository.ObterEndereco(enderecoId);
+
+                if (enderecoRemetente == null)
+                    throw new NotFoundException($"Não foi encontrado endereco de remetente para o pedido {codigoPedido}.");
+
+                return enderecoRemetente;
             }
             catch (Exception)
             {
